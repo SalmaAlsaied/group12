b35afb07a3c7a8dcba85428bd32d7170004e70d8
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-03-06 16:04:00 +0200

    Abdelkareem

---
+++ session1.php
@@ -0,0 +1,170 @@
+<?php 
+   
+   #  //   used to print message to user ... 
+   
+  /*
+   echo 
+   print 
+   message 
+   */
+
+ /*
+   echo "welcome to php course";
+   print("js course");
+   */
+
+
+
+
+//    $age = "test"; 
+
+// //    echo $age;
+//    var_dump($age);
+
+//  int
+//  float (doubles)
+//  bool 
+//  null 
+//  string 
+//  arrays 
+//  objects 
+//  resources 
+
+//    $age = 20; 
+
+   // code ..... 
+
+
+//    $age = 30; 
+
+// //    ECHO $AGE;  // 30  
+// ECHO $age;
+
+
+
+//  define("size",50,true);     // case insenstive   default = false .. 
+
+// //    echo size;
+
+//  echo  constant("SIZE");
+
+
+// const size = 50;
+//  echo size;
+
+
+
+// $num1 = 1; 
+// $num2 = 2; 
+
+// echo ($num1+$num2);
+/*
++ 
+- 
+* 
+/ 
+%
+**
+*/
+
+
+// $age = 20; 
+
+
+// $num1 = 2; 
+
+// // $num1 = $num1 + 5 ; 
+
+// $num1 += 5;    //   =   $num1 = $num1 + 5 ; 
+
+// $num1 = 5 ; 
+// $num2 = "5"; 
+
+/*
+ ==     (value)   
+ ===    (value && datatype )
+ !=     (value) 
+ !==    (value && datatype )
+ > 
+ < 
+*/
+
+//  var_dump($num1 !== $num2); 
+
+
+// $num1 = 5 ; 
+
+// // $num1 +=1; 
+
+// //  $num1++;   // post incremnt ... 
+
+// // echo $num1; // 6 
+
+// // echo   ++$num1;    // pre incremnt .... 
+
+// //  $num1--;
+// //  echo $num1;
+
+// echo --$num1;   // $num1 -=1;
+/*
+
+     COND1       AND (&&)       COND2    Result 
+     true        and            true      true  
+     true        and            fasle     false 
+     false       and            true      false 
+     false       and            false     false 
+
+
+
+
+     COND1       OR (||)        COND2    Result 
+     true        or             true      true   
+     true        or             fasle     true 
+     false       or             true      true 
+     false       or             false     false
+
+
+
+
+     COND1       xOR         COND2    Result 
+     true        xor         true      false  
+     true        xor         fasle     true
+     false       xor         true      true
+     false       xor         false     false
+
+    !(true)   = false 
+
+
+     */
+
+
+//    $name = "root"; 
+//    $text = "welcome"; 
+
+//    $message = $text.$name."&email = root@yahoo.com"."age = 11"; 
+
+//    echo $message;
+
+
+$status = 50; 
+
+  //    ()      {    }
+  if($status == 3){
+    echo "test Message";
+  }elseif($status ==  4){
+      echo "Status = 4";
+  }elseif($status ==  5){
+    echo "Status = 5";
+}
+  else{
+      echo "Status != 3 && 4 && 5 ";
+  }
+
+
+
+
+
+
+?>
+
+
